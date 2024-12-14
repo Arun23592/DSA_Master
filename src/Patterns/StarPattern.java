9d4660761e5eec3d87a919330731a4a96f6af4e2
Arun23592 <arunsubramani1992@gmail.com>
2024-12-14 23:23:41 +0530

    StarPattern

--- src/Patterns/StarPattern.java
+++ src/Patterns/StarPattern.java
@@ -7,7 +7,7 @@ public class StarPattern {
 
         for (int i=1; i<=n; i++){
             for (int j=1; j<=i; j++){
-                System.out.println("*");
+                System.out.print(" *");
             }
             System.out.println();
         }
