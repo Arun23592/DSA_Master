61b41db1deaf0d34d4386983ec122276c9380120
Arun23592 <arunsubramani1992@gmail.com>
2024-12-20 23:44:30 +0530

    FindDupNum

--- src/LeetcodePatterns/FindDuplicateNumber.java
+++ src/LeetcodePatterns/FindDuplicateNumber.java
@@ -2,5 +2,34 @@ package LeetcodePatterns;
 
 public class FindDuplicateNumber {
 
-    p
+    public int findDuplicates(int[] nums){
+        int left = 1;
+        int right = nums.length-1;
+        int duplicates = -1;
+
+        while (left<= right){
+            int mid = left +(right - left)/2;
+
+            int count = 0;
+            for (int num: nums){
+                if (num <= mid){
+                    count++;
+                }
+            }
+
+            if (count > mid){
+                duplicates =mid;
+                right = mid - 1;
+            }else {
+                left = mid + 1;
+            }
+        }
+        return duplicates;
+    }
+
+    public static void main(String[] args){
+        int[] nums = {1,3,4,2,2};
+        FindDuplicateNumber dup = new FindDuplicateNumber();
+        System.out.println("Duplicate Number: "+dup.findDuplicates(nums));
+    }
 }
