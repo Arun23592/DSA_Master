057e8d3c6166b8db5f3ddce712fe509afd6621ab
Arun23592 <arunsubramani1992@gmail.com>
2025-01-27 22:40:13 +0530

    countOccurrence

--- src/STRING/CountWordOccurrences.java
+++ src/STRING/CountWordOccurrences.java
@@ -1,2 +1,22 @@
-package STRING;public class CountWordOccurrences {
+package STRING;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountWordOccurrences {
+
+    public static void main(String[] args){
+        String input = "Hello world! Hello, Java. Welcome to the Java world.";
+        String[] words = input.toLowerCase().split("\\W+");
+        Map<String, Integer> wordCount = new HashMap<>();
+        for (String word: words){
+            if (!word.isEmpty()){
+                wordCount.put(word, wordCount.getOrDefault(word, 0)+1);
+            }
+        }
+        System.out.println("Word occurrence:");
+        for (Map.Entry<String, Integer> entry: wordCount.entrySet()){
+            System.out.println(entry.getKey() +": "+entry.getValue());
+        }
+    }
 }
