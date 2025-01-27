a605275d98aefca8c855a42426b6e2f5a86fc254
Arun23592 <arunsubramani1992@gmail.com>
2025-01-27 22:41:44 +0530

    convertToString

--- src/WorkOutSpace/ConvertStringToInteger.java
+++ src/WorkOutSpace/ConvertStringToInteger.java
@@ -1,2 +1,9 @@
-package WorkOutSpace;public class ConvertStringToInteger {
+package WorkOutSpace;
+
+public class ConvertStringToInteger {
+    public static void main(String[] args){
+        String str = "Two";
+        int number = Integer.parseInt(str);
+        System.out.println("Converted integer: "+number);
+    }
 }
