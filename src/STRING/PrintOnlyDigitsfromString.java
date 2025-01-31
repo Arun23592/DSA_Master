e772aca62773db0bbaa4ee56191fbc8852a224f7
Arun23592 <arunsubramani1992@gmail.com>
2025-01-31 22:58:58 +0530

    PrintonlyDigits

---
+++ src/STRING/PrintOnlyDigitsfromString.java
@@ -0,0 +1,32 @@
+package STRING;
+
+public class PrintOnlyDigitsfromString {
+
+    public static void main(String[] args){
+       String[] S = {"USD 89,989.00","INR 679,76.899"};
+
+       for (String str: S){
+           StringBuilder digitsOnly = new StringBuilder();
+
+           for (char ch: str.toCharArray()){
+               if (Character.isDigit(ch)){
+                   digitsOnly.append(ch);
+               }else if (ch == '.'){
+                   break;
+               }
+           }
+           System.out.print(digitsOnly+ " ");
+       }
+    }
+
+
+
+}
+
+
+
+
+
+//Input: S1 = {"USD 89,989.00","INR 679,76.899"}
+//
+//Output: 89989, 67976
