fa3d96fd18cd5278831b41861fda78aacb5a1e90
Arun23592 <arunsubramani1992@gmail.com>
2025-02-03 23:06:13 +0530

    printFirstChar

--- src/STRING/PrintFirstCharacterOfString.java
+++ src/STRING/PrintFirstCharacterOfString.java
@@ -1,4 +1,14 @@
 package STRING;
 
 public class PrintFirstCharacterOfString {
+
+    public static void main(String[] args){
+        String s = "Selenium Automation";
+        String[] words = s.split(" ");
+        for (String word: words){
+            if (!word.isEmpty()){
+                System.out.print(word.charAt(0)+ " ");
+            }
+        }
+    }
 }
