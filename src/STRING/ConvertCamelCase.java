ef9eabe8d5a0cfc1dfb1e860f164f6587a561ce5
arunsubramani <arunsubramani1992@gmail.com>
2025-03-05 23:30:54 +0530

    camelCase2

--- src/STRING/ConvertCamelCase.java
+++ src/STRING/ConvertCamelCase.java
@@ -4,6 +4,10 @@ public class ConvertCamelCase {
 
     public static String cameCase(String str){
 
+        if (str == null || str.isEmpty()){
+            return "";
+        }
+
         String[] words = str.split("[^a-zA-z]+");
         StringBuilder results = new StringBuilder();
 
@@ -11,15 +15,21 @@ public class ConvertCamelCase {
             String word = words[i].toLowerCase();
             if (i == 0){
                 results.append(word);
+            }else {
+                results.append(Character.toUpperCase(word.charAt(0))).append(word.substring(1));
             }
         }
+
+        return results.toString();
     }
 
 
 
 
     public static void main(String[] args){
-        String str = "cats AND*DogsareAwesome";
+        String str = "cats AND*Dogsare Awesome";
+       String cCase = cameCase(str);
+        System.out.println(cCase);
 
 
     }
