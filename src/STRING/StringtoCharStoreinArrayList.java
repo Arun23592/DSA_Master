2e47d3a4e7c511088bb087eff81d7187c06e74e2
arunsubramani <arunsubramani1992@gmail.com>
2025-08-21 23:27:33 +0530

    Stringchar

---
+++ src/STRING/StringtoCharStoreinArrayList.java
@@ -0,0 +1,24 @@
+package STRING;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringtoCharStoreinArrayList {
+
+    public static void main(String[] args){
+        String New = "Welcome to HCL interview";
+
+        String[] word = New.split("");
+
+        List<String> arr = new ArrayList<>();
+
+        for (String ch : word){
+            arr.add(ch);
+        }
+        System.out.println(arr);
+    }
+}
+
+/*
+* Split the string into characters and store it in arraylist
+* */
