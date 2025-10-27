1535322d3c6dfc16bafaa283ef437b51db7975f7
arunsubramani <arunsubramani1992@gmail.com>
2025-10-27 22:14:57 +0530

    MoveAllZerosRight

--- src/TwoPointers/MoveAllZerosRight.java
+++ src/TwoPointers/MoveAllZerosRight.java
@@ -4,20 +4,19 @@ import java.util.Arrays;
 
 public class MoveAllZerosRight {
 
-    public void moveZeros(int[] nums){
-        int start= nums.length, end= nums.length;
-        while (end >=0){
-            if (nums[end] == 0){
-                end++;
-            }else {
-                int temp = nums[end];
-                nums[end] = nums[start];
-                nums[start] = temp;
-                end++;
-                start++;
+    public static int[] moveZeros(int[] nums){
+
+        int position = 0;
+
+        for (int i=0; i<nums.length; i++){
+            if(nums[i] != 0){
+                int temp = nums[i];
+                nums[i] = nums[position];
+                nums[position] = temp;
+                position++;
             }
         }
-
+        return nums;
     }
 
     public static void main(String[] args){
