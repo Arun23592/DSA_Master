52a6bd0a638fce15e33b9e3d1a778582e56440b2
arunsubramani <arunsubramani1992@gmail.com>
2025-11-12 23:17:36 +0530

    countvowels

---
+++ src/STRING/CountVowels.java
@@ -0,0 +1,15 @@
+package STRING;
+
+public class CountVowels {
+
+    public static void main(String[] args){
+        String input ="user name";
+        int count = 0;
+
+        for (int i=0; i<input.length(); i++){
+            char ch = input.charAt(i);
+
+            if()
+        }
+    }
+}
