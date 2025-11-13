245bbe10a7af19b62b6013429bc47c47c465a5a1
arunsubramani <arunsubramani1992@gmail.com>
2025-11-13 23:34:00 +0530

    rotation

---
+++ src/STRING/CheckStringRotation.java
@@ -0,0 +1,13 @@
+package STRING;
+
+public class CheckStringRotation {
+
+    public static boolean areRotation(String s1, String s2){
+
+    }
+
+    public static void main(String[] args){
+        String s1 = "abcd";
+        String s2 = "cdab";
+    }
+}
