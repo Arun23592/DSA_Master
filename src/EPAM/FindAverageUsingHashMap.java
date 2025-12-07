7c674935285fc0a522e50501f41f6fd88c6dfe42
arunsubramani <arunsubramani1992@gmail.com>
2025-12-07 23:02:16 +0530

    findaverages

--- src/EPAM/FindAverageUsingHashMap.java
+++ src/EPAM/FindAverageUsingHashMap.java
@@ -1,4 +1,41 @@
 package EPAM;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FindAverageUsingHashMap {
+
+    public static void main(String[] args){
+        Map<String, Integer> data = new HashMap<>();
+
+        data.put("Brian", 21);
+        data.put("Lee", 22);
+        data.put("Cathe", 45);
+
+        System.out.println(data);
+
+        int sum = 0;
+        int count = data.size();
+        for (int val: data.values()){
+            sum += val;
+        }
+
+        double average = (double) sum / count;
+        System.out.println("Average :"+average);
+    }
 }
+
+
+
+/*
+*
+* Write a code using HashMap
+
+
+Brian – 21
+Lee – 22
+Cathe – 45
+
+Using key and value and find the average?
+*
+* */
