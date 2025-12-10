3c71b8b29b46ce7fd4eb01035f4b393264f35e29
arunsubramani <arunsubramani1992@gmail.com>
2025-12-10 22:29:36 +0530

    palindrome

--- src/ImportantTechnicalCodings/PalindromeString.java
+++ src/ImportantTechnicalCodings/PalindromeString.java
@@ -20,7 +20,7 @@ public class PalindromeString {
     }
 
     public static void main(String[] args){
-        String str = "Madams";
+        String str = "madams";
         System.out.println(isPalindromeString(str));
     }
 }
