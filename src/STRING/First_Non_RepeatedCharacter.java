016fdad86021879bb807b80a533cb0fc36c26e3f
arunsubramani <arunsubramani1992@gmail.com>
2025-12-16 23:57:49 +0530

    AllRepeatedCharacters

--- src/STRING/First_Non_RepeatedCharacter.java
+++ src/STRING/First_Non_RepeatedCharacter.java
@@ -1,5 +1,6 @@
 package STRING;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,10 +22,26 @@ public class First_Non_RepeatedCharacter {
         }
 
         //2. FInd the character with count equal to 1.
+        List<Character> nonRepeatedCharacter = new ArrayList<>();
+        for (Map.Entry<Character, Integer> entry: freqMap.entrySet()){
+            if(entry.getValue() == 1){
+                System.out.println(entry.getKey());
+            }
 
+        }
+
+        return nonRepeatedCharacter;
     }
 
     public static void main(String[] args){
         String input = "Is it your first company";
+
+        List<Character> results = firstNonRepeatedCharacter(input);
+
+        if (!results.isEmpty()){
+            System.out.println("All Non-Repeated character is: "+results.get(1));
+        }else {
+            System.out.println("Non-Repeated characters not found: ");
+        }
     }
 }
