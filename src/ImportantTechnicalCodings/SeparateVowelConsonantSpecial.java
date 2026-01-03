ea749d492537378a4442129580ef05ae5c02911d
arunsubramani <arunsubramani1992@gmail.com>
2026-01-03 23:31:23 +0530

    spearatevowels

---
+++ src/ImportantTechnicalCodings/SeparateVowelConsonantSpecial.java
@@ -0,0 +1,30 @@
+package ImportantTechnicalCodings;
+
+public class SeparateVowelConsonantSpecial {
+
+    public static void main(String[] args){
+        String input = "Hello@123";
+
+        StringBuilder vowels = new StringBuilder();
+        StringBuilder consonants = new StringBuilder();
+        StringBuilder Digit = new StringBuilder();
+        StringBuilder specials = new StringBuilder();
+
+
+        for(char ch: input.toCharArray()){
+            if (Character.isLetter(ch)){
+                if("AEIOUaeiou".indexOf(ch) != -1){
+                    vowels.append(ch);
+                }else {
+                    consonants.append(ch);
+                }
+            } else if (Character.isDigit(ch)) {
+                Digit.append(ch);
+            }else {
+
+            }
+
+        }
+
+    }
+}
