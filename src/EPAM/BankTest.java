71590324dd6e9bc8162a087a25a37fc5466b5eab
arunsubramani <arunsubramani1992@gmail.com>
2026-01-10 05:58:58 +0530

    epam

--- src/EPAM/BankTest.java
+++ src/EPAM/BankTest.java
@@ -1,4 +1,8 @@
 package EPAM;
 
 public class BankTest {
+
+        public static void main(String[] args){
+
+        }
 }
---
+++ src/EPAM/RemoveDuplicates.java
@@ -0,0 +1,28 @@
+package EPAM;
+
+import java.util.LinkedHashSet;
+
+public class RemoveDuplicates {
+
+    public static String removeDuplicates(String s){
+        LinkedHashSet<Character> set = new LinkedHashSet<>();
+
+        for(char c: s.toCharArray()){
+            set.add(c);
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        for(char c: set){
+            result.append(c);
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args){
+        String s = "programming";
+        String removed = removeDuplicates(s);
+        System.out.println(removed);
+    }
+}
