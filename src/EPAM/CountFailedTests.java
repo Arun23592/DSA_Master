8422b02a7890b35e5201701fa15b3cf2a2f4bf9d
arunsubramani <arunsubramani1992@gmail.com>
2026-01-12 23:36:48 +0530

    countFail

--- src/EPAM/CountFailedTests.java
+++ src/EPAM/CountFailedTests.java
@@ -1,4 +1,58 @@
 package EPAM;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class CountFailedTests {
+
+    public static void main(String[] args){
+        List<String> testResults = Arrays.asList(
+                "Logintest - PASS",
+                "CheckoutTest - FAIL",
+                "AddCartTest - FAIL",
+                "PaymentTest - PASS"
+        );
+
+        List<String> failedTests = new ArrayList<>();
+        int count = 0;
+
+
+        for(String tests: testResults){
+            if (tests.contains("FAIL")){
+                failedTests.add(tests);
+                count++;
+            }
+        }
+
+        System.out.println("Failed test cases: "+failedTests);
+        System.out.println("count is: " +count);
+
+    }
+
+
 }
+
+
+
+/**
+ *
+ * write a Java program for the below scenario:
+ * List of test cases which contains PASS and FAIL, and you need to count the number of tescases failed and fetch the failed testcases names.
+ *
+ * List = ["Logintest - PASS",
+ *            "CheckoutTest - FAIL",
+ * 		"AddCartTest - FAIL",
+ * 		"PaymentTest - PASS"
+ * 	   ]
+ *
+ *
+ * Output:
+ * [
+ *  "CheckoutTest - FAIL",
+ * "AddCartTest - FAIL",
+ * ]
+ *
+ * count is 2.
+ *
+ * */
