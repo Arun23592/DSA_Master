793b6c75f2ae0bca56e5c0ba3df29e5f7009bd3d
arunsubramani <arunsubramani1992@gmail.com>
2026-01-16 23:45:11 +0530

    palindrome

--- src/comcast/checkPalindromeString.java
+++ src/comcast/checkPalindromeString.java
@@ -1,4 +1,29 @@
 package comcast;
 
 public class checkPalindromeString {
+
+    public static boolean isPalindrome(String s){
+    String str = s.toLowerCase();
+
+    String st = str.replaceAll("[^a-zA-Z0-9]", "");
+
+    int left = 0;
+    int right = st.length()-1;
+
+    while(left < right){
+        if (st.charAt(left) != st.charAt(right)){
+            return false;
+
+        }
+        left++;
+        right--;
+    }
+    return true;
+
+    }
+
+    public static void main(String[] args){
+        String s = "madam";
+        System.out.println(isPalindrome(s));
+    }
 }
