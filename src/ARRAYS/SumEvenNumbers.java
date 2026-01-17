433c165f96b94283fb707cfb1a84345f51a67d80
arunsubramani <arunsubramani1992@gmail.com>
2026-01-17 22:44:14 +0530

    sumevennum

--- src/ARRAYS/SumEvenNumbers.java
+++ src/ARRAYS/SumEvenNumbers.java
@@ -1,4 +1,18 @@
 package ARRAYS;
 
 public class SumEvenNumbers {
+    public static void main(String[] args){
+        int[] nums = {1, 2, 3, 4, 5, 6};
+
+        int sum = 0;
+
+        for(int arr: nums){
+            if(arr % 2 == 0){
+                sum += arr;
+            }
+
+        }
+
+        System.out.println("sum of even numbers: "+sum);
+    }
 }
