6451c46865eabadedae7cc134b0356ae6423a274
arunsubramani <arunsubramani1992@gmail.com>
2026-01-17 22:43:51 +0530

    sortArray

---
+++ src/ARRAYS/Zigzag_SortArray.java
@@ -0,0 +1,36 @@
+package ARRAYS;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Zigzag_SortArray {
+
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        List<Integer> result = new ArrayList<>();
+
+        while (left <= right){
+            result.add(arr[left++]);
+            if (left <= right){
+                result.add(arr[right--]);
+            }
+        }
+
+        for (int num: result){
+            System.out.print(num+" ");
+        }
+    }
+}
+
+
+/**
+ *
+ * input: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+ * output: 1, 10, 2, 9, 3, 8, 4, 7, 5, 6
+ *
+ * App
+ * */
