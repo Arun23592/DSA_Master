16c8be539a83aae0898c4a171514751f6d8a8fb5
arunsubramani <arunsubramani1992@gmail.com>
2026-01-18 23:19:04 +0530

    sortArray

--- src/ARRAYS/Zigzag_SortArray.java
+++ src/ARRAYS/Zigzag_SortArray.java
@@ -32,5 +32,15 @@ public class Zigzag_SortArray {
  * input: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
  * output: 1, 10, 2, 9, 3, 8, 4, 7, 5, 6
  *
- * App
+ * Approach: Two Pointers
+ *
+ * Sort the array (though in your case it’s already sorted).
+ *
+ * Use two pointers:
+ *
+ * left → start of array (smallest).
+ *
+ * right → end of array (largest).
+ *
+ * Alternate picking from left and right until they meet.
  * */
