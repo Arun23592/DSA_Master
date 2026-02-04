812d01a463f7c045894d2d03f51907fa181de9ce
arunsubramani <arunsubramani1992@gmail.com>
2026-02-04 22:03:10 +0530

    Expandion

--- src/STRING/StringExpansion.java
+++ src/STRING/StringExpansion.java
@@ -10,17 +10,23 @@ public class StringExpansion {
         StringBuilder result = new StringBuilder();
 
         int length = str.length();
+
         for (int i=0; i<length; i++){
             char currentChar = str.charAt(i);
             int count = 0;
             while (i+1 < length && Character.isDigit(str.charAt(i+1))){
                 count = count * 10 + (str.charAt(++i) - '0');
             }
+
             for (int j=0; j<count; j++){
                 result.append(currentChar);
             }
+
         }
         return result.toString();
+
+
+
     }
 
     public static void main(String[] args){
