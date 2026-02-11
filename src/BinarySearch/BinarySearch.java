e2e18a9cbd0529411a942064b89aa747526e25b0
arunsubramani <arunsubramani1992@gmail.com>
2026-02-11 22:26:38 +0530

    binarysearch

---
+++ src/BinarySearch/BinarySearch.java
@@ -0,0 +1,9 @@
+package BinarySearch;
+
+public class BinarySearch {
+
+    public static void main(String[] args){
+        int[] arr = {3, 8, 15, 23, 42, 56, 78};
+        int target = 23;
+    }
+}
