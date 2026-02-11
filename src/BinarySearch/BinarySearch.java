238f54db931fddf0e2fb0d120c20411b7c5d0a3e
arunsubramani <arunsubramani1992@gmail.com>
2026-02-11 22:34:42 +0530

    binarysearch2

--- src/BinarySearch/BinarySearch.java
+++ src/BinarySearch/BinarySearch.java
@@ -2,8 +2,45 @@ package BinarySearch;
 
 public class BinarySearch {
 
+    public static int binarySearch(int[] arr, int target){
+        int low = 0;
+        int high = arr.length - 1;
+
+        while(low <= high){
+            int mid = low + high / 2;
+
+            if(arr[mid] == target){
+                return mid;
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            }else {
+                low = mid - 1;
+            }
+        }
+        return -1;
+    }
     public static void main(String[] args){
         int[] arr = {3, 8, 15, 23, 42, 56, 78};
         int target = 23;
+
+        int result = binarySearch(arr, target);
+        System.out.println(result != -1 ? "Found at index: " +result : "Not found");
     }
 }
+
+/**
+ *
+ * What Is Binary Search?
+ * Binary Search works by repeatedly dividing the search range in half. It compares the target value to the middle element of the array:
+ *
+ * If they match, the search is complete.
+ *
+ * If the target is smaller, it searches the left half.
+ *
+ * If the target is larger, it searches the right half.
+ *
+ * This continues until the element is found or the range is empty.
+ *
+ *
+ *
+ * */
