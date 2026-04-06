233bf0216da8e3e6d87fe30f7f8cf7d274acfddc
arunsubramani <arunsubramani1992@gmail.com>
2026-04-06 23:33:36 +0530

    revefsewordkeppspec

--- src/STRING/ReverseWordsKeepSpecialChar.java
+++ src/STRING/ReverseWordsKeepSpecialChar.java
@@ -1,4 +1,53 @@
 package STRING;
 
 public class ReverseWordsKeepSpecialChar {
+
+    public static String reverseKeepSpecial(String input){
+
+
+        char[] arr = input.toCharArray();
+        int left = 0;
+        int right = arr.length - 1;
+
+
+        while(left < right){
+            if(!Character.isLetterOrDigit(arr[left])){
+                left++;
+            }else if(!Character.isLetterOrDigit(arr[right])){
+                right--;
+            }else {
+                char temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+        return new String(arr);
+
+    }
+
+    public static void main(String[] args){
+        String input = "ab-cd!ef";
+        System.out.println(reverseKeepSpecial(input));
+    }
 }
+
+/**
+ * Approach implemented:
+ *
+ * Two-pointer technique
+ * Skip special characters
+ * Swap only letters/digits
+ * Time complexity:
+ *
+ * O(n)
+ * Space complexity:
+ *
+ * O(n) because of the char array copy
+ * Extra auxiliary logic is O(1)
+ *
+ *
+ *
+ * **/
