ad55725dc112b7638e7a7e3f957facc5df203a00
arunsubramani <arunsubramani1992@gmail.com>
2026-04-11 23:38:07 +0530

    reversewordspec

--- src/STRING/reverseWordsPreserveSpecial.java
+++ src/STRING/reverseWordsPreserveSpecial.java
@@ -2,7 +2,36 @@ package STRING;
 
 public class reverseWordsPreserveSpecial {
 
+
+    public  static  String reverseWordsKeepspecialChars(String str){
+
+        char[] chars = str.toCharArray();
+
+        int left = 0, right = chars.length - 1;
+
+        while (left < right){
+
+            while (left < right && !Character.isLetterOrDigit(chars[left])){
+                left++;
+            }
+
+            while (left < right && !Character.isLetterOrDigit(chars[right])){
+                right--;
+            }
+
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+            left++;
+            right--;
+        }
+
+        return new String(chars);
+    }
     public static void main(String[] args){
-        String
+        String str = "a@b#c";
+
+        String result = reverseWordsKeepspecialChars(str);
+        System.out.println(result);
     }
 }
