c2445148a071b651294c392c2e61887588f46b6f
arunsubramani <arunsubramani1992@gmail.com>
2026-04-15 23:53:44 +0530

    reverse

--- src/STRING/reverseWordsPreserveSpecial.java
+++ src/STRING/reverseWordsPreserveSpecial.java
@@ -7,31 +7,51 @@ public class reverseWordsPreserveSpecial {
 
         char[] chars = str.toCharArray();
 
-        int left = 0, right = chars.length - 1;
+        int left = 0, right = chars.length;
 
         while (left < right){
 
-            while (left < right && !Character.isLetterOrDigit(chars[left])){
+            while (!Character.isLetterOrDigit(chars[left])){
+                left++;
+                continue;
+            }
+
+            int wordStart = left;
+            while (left < right && Character.isLetterOrDigit(chars[left])){
                 left++;
             }
 
-            while (left < right && !Character.isLetterOrDigit(chars[right])){
-                right--;
+            int wordEnd = left - 1;
+
+            int i = wordStart, n = wordEnd;
+            while (i < n){
+                char temp = chars[i];
+                chars[i] = chars[n];
+                chars[n] = temp;
+                i++;
+                n--;
             }
 
-            char temp = chars[left];
-            chars[left] = chars[right];
-            chars[right] = temp;
-            left++;
-            right--;
         }
 
         return new String(chars);
     }
     public static void main(String[] args){
-        String str = "a@b#c";
+        String str = "Hello@World#Java";
 
         String result = reverseWordsKeepspecialChars(str);
         System.out.println(result);
     }
 }
+
+/**
+ *
+ * Q: Reverse a string of words by splitting it where we find special character & ensure special character position is intact.
+ *
+ * Approach 1 — Two-Pointer (Most Efficient) ✅
+ *
+ *
+ *
+ *
+ *
+ * */
