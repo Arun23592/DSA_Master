f9c737f35833ab94590896cd29e8ffb29ec90fe4
arunsubramani <arunsubramani1992@gmail.com>
2026-05-29 23:01:23 +0530

    missing

--- src/ARRAYS/MissingNumber.java
+++ src/ARRAYS/MissingNumber.java
@@ -14,7 +14,7 @@ public class MissingNumber {
 
 
     public static void main(String[] args){
-        int[] nums = {9,6,4,2,3,5,7,0,1};
+        int[] nums = {9,6,2,3,5,7,0,1};
         MissingNumber miss = new MissingNumber();
         int missing = miss.missingNumber(nums);
         System.out.println("Missing number is : "+missing);
