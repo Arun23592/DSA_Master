ae7e9253e05f4676b12bf2c9ce113da738295904
arunsubramani <arunsubramani1992@gmail.com>
2026-09-06 17:28:47 +0530

    findvalidlinks

---
+++ src/ComplexCodes/FindValidInvalidLinks.java
@@ -0,0 +1,21 @@
+package ComplexCodes;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FindValidInvalidLinks {
+
+
+
+    public static void main(String[] args){
+
+        List<String> links = Arrays.asList(
+
+                "https://www.example.com",
+                "https://www.google.co.in/",
+                "https://www.invalidlink.com/abc",
+                "https://www.github.com",
+                "https://www.thiswebsitedoesnotexist12345.com"
+        );
+    }
+}
