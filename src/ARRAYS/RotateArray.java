5d7552ae4992c05389fe2667650d09c9e658dd12
arunsubramani <arunsubramani1992@gmail.com>
2026-09-08 23:49:01 +0530

    rotate

--- src/ARRAYS/RotateArray.java
+++ src/ARRAYS/RotateArray.java
@@ -24,7 +24,7 @@ public class RotateArray {
 
     public static void main(String[] args){
         int[] nums = {1, 2, 3, 4, 5, 6, 7};
-        int k = 1;
+        int k = 3;
         RotateArray rot = new RotateArray();
         rot.rotate(nums, k);
         System.out.println("Rotated Array: "+ Arrays.toString(nums));
