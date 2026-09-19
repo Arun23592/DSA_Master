63f2abe32b43d71c5bd0814b5574e12ff69f771e
arunsubramani <arunsubramani1992@gmail.com>
2026-09-19 22:07:12 +0530

    suffix

---
+++ src/ComplexCodes/SuffixMatchingPrefix.java
@@ -0,0 +1,9 @@
+package ComplexCodes;
+
+public class SuffixMatchingPrefix {
+
+
+    public static void main(String[] args){
+        String[] match = {"A....*", "*====#", "#____X", "X____Z"};
+    }
+}
